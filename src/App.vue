8408e992bfb0153a9940e380ee6880ff2ef46589
wonderl17 <809646531@qq.com>
2020-03-18 11:59:24 +0800

    项目目录

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios'
 import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   },
   data(){
     return{
@@ -22,15 +22,7 @@ export default {
     }
   },
   mounted(){
-    let url = "/api/common/adver-getadver"
-    jsonp(url,(err,res)=>{
-        let result = res;
-        this.data1 = result;
-    })
-    axios(url).then((res)=>{
-        let result = res;
-        this.data2 = result;
-    })
+    
   }
 }
 </script>
