ee436229eede188e917658b644edaec612cbc9e1
wonderl17 <809646531@qq.com>
2020-03-19 21:14:00 +0800

    环境设置

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import VueAxios from 'vue-axios'
 
 
 import App from './App.vue'
+import env from './env'
+
 
 //根据前端的跨域方式做调整 
 axios.defaults.baseURL = '/api'
 axios.defaults.timeout = 8000;
+axios.defaults.baseURL = env.baseURL
 //接口错误拦截
 axios.interceptors.response.use(function (response) {
   let res = response.data;
