cd25f99dc3d08e35e264f2dd3198bd902dbd6d7d
wonderl17 <809646531@qq.com>
2020-03-23 18:14:19 +0800

    数据渲染

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -9,11 +9,13 @@
             <a href="javascript:;">协议规则</a>
           </div>
           <div class="topbar-user">
-            <a href="javascript:;">登录</a>
-            <a href="javascript:;">注册</a>
-            <a href="javascript:;" class="my-cart"><span class="icon-cart">
+            <a href="javascript:;" v-if="username">{{username}}</a>
+            <a href="javascript:;" v-if="!username" @click="login">登陆</a>
+
+            <a href="javascript:;" v-if="username">我的订单</a> 
+            <a href="javascript:;" class="my-cart" @click="goToCart"><span class="icon-cart">
               </span>购物车</a>
-          </div>
+          </div>  
         </div>
     </div>
     <div class="nav-header">
@@ -26,60 +28,16 @@
             <span>小米手机</span>
             <div class="children">
               <ul>
-                <li class="product">
-                  <a href="" target="_blank">
+                <li class="product" v-for="(item,index) in phoneList" :key="index">
+                  <a :href="'/#/product/'+ item.id" target="_blank">
                     <div class="pro-img">
-                      <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/82ddffd7562c54f9166fa876c143ff22.png?thumb=1&w=200&h=138&f=webp&q=90" alt="">
+                      <img :src="item.mainImage" :alt="item.subtitle">
                     </div>
-                    <div class="pro-name">小米10 Pro</div>
-                    <div class="pro-price">4999元起</div>
+                    <div class="pro-name">{{item.name}}</div>
+                    <div class="pro-price">{{item.price |currency}}</div>
                   </a>
                 </li>
-                <li class="product">
-                  <a href="" target="_blank">
-                    <div class="pro-img">
-                      <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/82ddffd7562c54f9166fa876c143ff22.png?thumb=1&w=200&h=138&f=webp&q=90" alt="">
-                    </div>
-                    <div class="pro-name">小米10 Pro</div>
-                    <div class="pro-price">4999元起</div>
-                  </a>
-                </li>
-                <li class="product">
-                  <a href="" target="_blank">
-                    <div class="pro-img">
-                      <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/82ddffd7562c54f9166fa876c143ff22.png?thumb=1&w=200&h=138&f=webp&q=90" alt="">
-                    </div>
-                    <div class="pro-name">小米10 Pro</div>
-                    <div class="pro-price">4999元起</div>
-                  </a>
-                </li>
-                <li class="product">
-                  <a href="" target="_blank">
-                    <div class="pro-img">
-                      <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/82ddffd7562c54f9166fa876c143ff22.png?thumb=1&w=200&h=138&f=webp&q=90" alt="">
-                    </div>
-                    <div class="pro-name">小米10 Pro</div>
-                    <div class="pro-price">4999元起</div>
-                  </a>
-                </li>
-                <li class="product">
-                  <a href="" target="_blank">
-                    <div class="pro-img">
-                      <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/82ddffd7562c54f9166fa876c143ff22.png?thumb=1&w=200&h=138&f=webp&q=90" alt="">
-                    </div>
-                    <div class="pro-name">小米10 Pro</div>
-                    <div class="pro-price">4999元起</div>
-                  </a>
-                </li>
-                <li class="product">
-                  <a href="" target="_blank">
-                    <div class="pro-img">
-                      <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/82ddffd7562c54f9166fa876c143ff22.png?thumb=1&w=200&h=138&f=webp&q=90" alt="">
-                    </div>
-                    <div class="pro-name">小米10 Pro</div>
-                    <div class="pro-price">4999元起</div>
-                  </a>
-                </li>
+               
               </ul>
             </div>
           </div>
@@ -166,24 +124,39 @@ export default {
     name:'nav-header',
     data(){
       return{
-        username: 'ww',
+        username: '',
         phoneList:[]
       } 
+    },
+    filters:{
+      currency(val){
+        if(!val) return '0.00';
+        return '￥'+val.toFixed(2)+'元';
+      }
     },
     mounted(){
       this.getProductList();
     },
     methods:{
+      login(){
+        this.$router.push('/login')
+      },
       getProductList(){
         this.axios.get('/products',{
           params:{
-            categoryId:'100012'
+            categoryId:'100012',
+            pageSize:''
           }
         }).then((res)=>{
-          if(res.list>6){
+          if(res.list.length>6){
             this.phoneList = res.list.slice(0,6);
+          }else{
+            this.phoneList = res.list
           }
         })
+      },
+      goToCart(){
+        this.$router.push('/cart')
       }
     }
 }
